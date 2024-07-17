68f2b3723df1760e6a551c03a9ac239ce60ef7d1
Jugal Kothari <jugalprakashk19@gmail.com>
2024-07-18 02:28:13 +0530

    Adds welcome message and dark mode toggle

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import { Vortex } from './ui/vortex'
+import LampWelcome from './Welcome';
+import { ModeToggle } from './ModeToggle';
 
 const HeroSection = () => {
   return (
     <div>
-        <Vortex
-        backgroundColor="black"
-        rangeY={800}
-        particleCount={500}
-        baseHue={120}
-        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
-        >
-        <h2>Welcome babes</h2>
-        </Vortex>
+        <nav>Hello <ModeToggle/></nav>
+        <LampWelcome/>
+        
     </div>
   )
 }
