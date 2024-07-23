c8bce407b0051292f07e27b066bc3df035d695ea
Jugal Kothari <jugalprakashk19@gmail.com>
2024-07-23 13:55:08 +0530

    Adds the resume as an image and applies styling

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Resume = React.forwardRef<HTMLDivElement>((_, ref) => {
   return (
-    <section id="resume" ref={ref} className="grey-section">
+    <section id="resume" ref={ref}>
         <div className="row section-intro">
-   		    <div className="col-twelve">
-
+   		    <div>
                 <h5>Resume</h5>
-                <h3 className="font-serif text-3xl mb-2">More of my credentials.</h3>
-
-   		    </div>   		
+                <h3 className="font-serif text-3xl">More about my experiences.</h3>
+   		    </div>
    	    </div>
+        <div className='flex justify-center items-center'>
+            <Image src="/JugalKothariResumeNew.jpg"
+            alt='Resume of the author'
+            width={600}
+            height={600}
+            className="rounded-sm shadow-lg shadow-gray-500"/>
+        </div>
     </section>
   )
 });
