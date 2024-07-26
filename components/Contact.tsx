031127a1069ab42ef61e27778ffb2182679167a4
Jugal Kothari <jugalprakashk19@gmail.com>
2024-07-26 21:20:05 +0530

    Adds form to contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,36 +1,15 @@
 import { color } from 'framer-motion';
 import React, { useState } from 'react';
+import { ContactForm } from './ContactForm';
 
 const Contact = React.forwardRef<HTMLDivElement>((_, ref) => {
-  const [contactName, setContactName] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
-  const [contactSubject, setContactSubject] = useState('');
-  const [contactMessage, setContactMessage] = useState('');
-  const [formStatus, setFormStatus] = useState({ isSending: false, isSuccess: false, isError: false });
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setFormStatus({ isSending: true, isSuccess: false, isError: false });
-
-    // Simulating form submission
-    setTimeout(() => {
-      setFormStatus({ isSending: false, isSuccess: true, isError: false });
-      setContactName('');
-      setContactEmail('');
-      setContactSubject('');
-      setContactMessage('');
-    }, 2000);
-  };
-
   return (
     <section id="contact">
       <div className="row section-intro">
           <h3>Contact</h3>
           <h1>I&apos;d Love To Hear From You.</h1>
-          <p className="lead">
-            Lorem ipsum Do commodo in proident enim in dolor cupidatat adipisicing dolore officia nisi aliqua incididunt Ut veniam lorem ipsum Consectetur ut in in eu do.
-          </p>
       </div>
+      <ContactForm />
     </section>
   );
 });
