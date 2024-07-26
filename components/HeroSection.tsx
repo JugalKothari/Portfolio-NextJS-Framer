0f193716f2f7c095d986ef0bf42f41e753ce1730
Jugal Kothari <jugalprakashk19@gmail.com>
2024-07-26 19:43:36 +0530

    Links icons to profiles and tooltip for discord

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord, faGithubSquare, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { Button } from './ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 const HeroSection = ({ scrollToAbout }: { scrollToAbout: () => void }) => {
+  
+  const socialLinks = {
+    github: "https://github.com/JugalKothari",
+    linkedin: "https://www.linkedin.com/in/jugal-kothari-b43136249/",
+    instagram: "https://www.instagram.com/jugalll?igsh=eThsNno3NjY4NzZv",
+    discord: "https://www.discordapp.com/users/472011275954683924"
+  };
+
   return (
     <section id='intro'>
       <div className='intro-overlay'></div>
@@ -31,10 +46,19 @@ const HeroSection = ({ scrollToAbout }: { scrollToAbout: () => void }) => {
       </div>
 
       <ul className="intro-social">        
-        <li><FontAwesomeIcon icon={faGithubSquare} /></li>
-        <li><FontAwesomeIcon icon={faLinkedinIn} /></li>
-        <li><FontAwesomeIcon icon={faInstagram} /></li>
-        <li><FontAwesomeIcon icon={faDiscord} /></li>
+        <li><a href={socialLinks.github} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faGithubSquare} /></a></li>
+        <li><a href={socialLinks.linkedin} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+        <li><a href={socialLinks.instagram} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faInstagram} /></a></li>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <li><FontAwesomeIcon icon={faDiscord} /></li>
+            </TooltipTrigger>
+            <TooltipContent className='bg-black text-white border-none'>
+              <p>jugalalala</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </ul> 
     </section>
   )
