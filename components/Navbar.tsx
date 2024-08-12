93761afa1e30a19eccf2fc9984b9e7219692a07d
Jugal Kothari <jugalprakashk19@gmail.com>
2024-08-12 21:50:30 +0530

    Makes navbar responsive for mobile screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="fixed w-full bg-black text-white z-10">
-      <ul className="flex justify-center space-x-4 py-4">
+      <ul className="flex justify-center space-x-4 py-4 text-xs sm:text-base">
         <li className="flex items-center">
-          <a href="#home" className="px-4 hover:text-pink-600">Home</a>
+          <a href="#home" className="px-0 sm:px-4 hover:text-pink-600">Home</a>
         </li>
         <span className="text-pink-600">|</span>
         <li className="flex items-center">
-          <a href="#about" className="px-4 hover:text-pink-600">About</a>
+          <a href="#about" className="px-0 sm:px-4 hover:text-pink-600">About</a>
         </li>
         <span className="text-pink-600">|</span>
         <li className="flex items-center">
-          <a href="#resume" className="px-4 hover:text-pink-600">Resume</a>
+          <a href="#resume" className="px-0 sm:px-4 hover:text-pink-600">Resume</a>
         </li>
         <span className="text-pink-600">|</span>
         <li className="flex items-center">
-          <a href="#projects" className="px-4 hover:text-pink-600">Projects</a>
+          <a href="#projects" className="px-0 sm:px-4 hover:text-pink-600">Projects</a>
         </li>
         <span className="text-pink-600">|</span>
         <li className="flex items-center">
-          <a href="#contact" className="px-4 hover:text-pink-600">Contact</a>
+          <a href="#contact" className="px-0 sm:px-4 hover:text-pink-600">Contact</a>
         </li>
       </ul>
     </nav>
