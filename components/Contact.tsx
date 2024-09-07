34408308b5caf47613256a2f590a274a00fe53c4
Jugal Kothari <jugalprakashk19@gmail.com>
2024-09-07 23:07:38 +0530

    Adds message for alternative contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,6 +7,7 @@ const Contact = React.forwardRef<HTMLDivElement>((_, ref) => {
       <div className="row section-intro">
           <h3>Contact</h3>
           <h1>I&apos;d Love To Hear From You.</h1>
+          <h5>Feel free to ping me on social media if I go MIA - don&apos;t let me ghost you!😉</h5>
       </div>
       <ContactForm />
     </section>
